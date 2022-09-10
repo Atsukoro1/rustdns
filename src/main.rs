b8b1da21c3915f414e87a86315e3970e6ee1faab
Atsukoro1 <dornicakkuba@gmail.com>
2022-09-10 22:48:42 +0200

    Neutral face

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,27 @@
-mod args;
+use std::str;
+use std::thread;
+use std::net::UdpSocket;
 
 fn main() {
-    println!("Hello, world!");
+
+    let socket = match UdpSocket::bind("192.168.0.15:22") {
+        Ok(s) => s,
+        Err(e) => panic!("couldn't bind socket: {}", e)
+    };
+
+    let mut buf = [0; 2048];
+    loop {
+        match socket.recv_from(&mut buf) {
+            Ok((amt, src)) => {
+                thread::spawn(move || {
+                    println!("amt: {}", amt);
+                    println!("src: {}", src);
+                    println!("{}", str::from_utf8(&buf).unwrap_or(""));
+                });
+            },
+            Err(e) => {
+                println!("couldn't recieve a datagram: {}", e);
+            }
+        }
+    }
 }
