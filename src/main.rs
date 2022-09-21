5dd813e2897e190bf5e066f06a0ba293b8386bf5
Atsukoro1 <dornicakkuba@gmail.com>
2022-09-21 12:32:53 +0200

    Logging

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 #[macro_use] 
 extern crate enum_primitive;
+extern crate slog_async;
+extern crate slog_term;
 extern crate slog;
 extern crate bit;
 
 use lazy_static::lazy_static;
-use slog::info;
+use slog::{o, Drain, info, crit};
 use tokio::sync::{MutexGuard, Mutex};
 use std::net::{UdpSocket, SocketAddr};
 use std::thread;
@@ -20,6 +22,23 @@ lazy_static! {
         helpers::config::load_config()
     };
 
+    pub static ref LOGGER: slog::Logger = {
+        let decorator = slog_term::TermDecorator::new()
+            .stdout()
+            .force_color()
+            .build();
+
+        let drain = slog_term::CompactFormat::new(decorator)
+            .build()
+            .fuse();
+
+        let drain = slog_async::Async::new(drain)
+            .build()
+            .fuse();
+    
+        slog::Logger::root(drain, o!("RUSTDNS" => "Rust dns is running!"))
+    };
+
     pub static ref SOCKET: UdpSocket = {
         let sckt = match UdpSocket::bind(
             format!("{}:{}", CONFIG.hostname, CONFIG.port)
@@ -28,7 +47,11 @@ lazy_static! {
                 .unwrap()
         ) {
             Ok(s) => {
-                println!("UDP socket succefully started!");
+                info!(
+                    LOGGER, 
+                    "UDP socket is running!";
+                    "host" => format!("{}:{}", CONFIG.hostname, CONFIG.port)
+                );
                 s
             },
             
@@ -42,8 +65,11 @@ lazy_static! {
         let mut manager = CacheManager::new();
 
         match manager.connect() {
-            Ok(_) => println!("Cache manager started!"),
-            Err(e) => panic!("Failed to start cache manager, {}", e)
+            Ok(_) => info!(LOGGER, "Cache manager succefully started!"),
+            Err(e) => {
+                crit!(LOGGER, "Failed to start cache manager!"; "Error" => e);
+                panic!()
+            }
         }
 
         manager
