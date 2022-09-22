3d9a9b77ac8100b4058e7ab3aed28fb66a167985
Atsukoro1 <dornicakkuba@gmail.com>
2022-09-22 22:56:36 +0200

    Improvements

--- src/parser/defs/dns.rs
+++ src/parser/defs/dns.rs
@@ -21,23 +21,20 @@ pub struct DNS {
 }
 
 pub trait Construct {
-    /// Create a new DNS struct with some default values
     fn new() -> DNS;
-
-    /// Parse raw bytes into DNS struct
-    /// 
-    /// As described at https://datatracker.ietf.org/doc/html/rfc1035 under
-    /// the 4.1.1 Header section format
-    /// 
-    /// Returns a Result that can contain DNS struct or an error code with 
-    /// specific error that will be contained in invalid response
     fn from(bytes: &[u8]) -> Result<DNS, ResponseCode>;
-
-    /// Convert the struct into bytes
     fn bytes(self) -> Result<Vec<u8>, ResponseCode>;
 }
 
 impl Construct for DNS {
+    fn from(bytes: &[u8]) -> Result<DNS, ResponseCode> {
+        parse_datagram(bytes)
+    }
+
+    fn bytes(self) -> Result<Vec<u8>, ResponseCode> {
+        datagram_bytes(self)
+    }
+
     fn new() -> DNS {
         DNS { 
             header: DNSHeader { 
@@ -59,13 +56,5 @@ impl Construct for DNS {
             authority: None,
             additional: None
         }
-    }
-
-    fn from(bytes: &[u8]) -> Result<DNS, ResponseCode> {
-        parse_datagram(bytes)
-    }
-
-    fn bytes(self) -> Result<Vec<u8>, ResponseCode> {
-        datagram_bytes(self)
     }
 }
